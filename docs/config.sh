5de827bfcc1d320f3ecd204c850187c92855b9d1
Larysha <60649802+Larysha@users.noreply.github.com>
2022-09-30 12:04:12 +0200

    Create config.sh

---
+++ docs/config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash/
+
+wkd1=/mnt/lustre/users/lrothmann/shotgun_meta
+wkd2=/mnt/lustre/users/lrothmann/shotgun_meta/work
+
+### Directories
+
+data=${wkd1}/raw/
+f=${wkd2}/Flye/
+qc=${wkd2}/LongQC/
+k=${wkd2}/Kraken2/
+b=${wkd2}/Busco/
+q=${wkd2}/Quast/
+kro=${wkd2}/Krona/
+
+### Software
+
+soft=${wkd}/softs/
